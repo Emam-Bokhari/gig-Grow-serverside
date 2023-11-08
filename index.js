e614fe47c586a83189b1c9614bcdf610e0eee835
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2023-11-08 19:31:32 +0600

    Remove unused file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const jwt = require("jsonwebtoken")
 const cookieParser = require('cookie-parser')
 require("dotenv").config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const app = express()
 
 // middleware
